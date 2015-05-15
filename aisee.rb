1e60f6797dc8af0dd98417ccf55a8600e7aab060
Ian Ellis <ellisia@me.com>
2015-05-15 23:18:32 +0100

    got rid of test aisee2.rb and put the iSight photo taking in aisee.rb. Messed around trying to get noise, and made the text output a single "image"

--- aisee.rb
+++ aisee.rb
@@ -73,8 +73,81 @@ end
 
 class InputSource
   def initialize()
-    @img = Magick::Image.read('triangle.png')[0]
+    if ARGV.length > 0
+      img2 = Magick::Image.read(ARGV[0])[0]
+    else
+      system "/usr/local/bin/imagesnap image.png"
+      img1 = Magick::Image.read('image.png')[0]
+      img2 = img1.resize_to_fit(320,240)
+    end
+# Convert to black & white
+# First find the lowest maximum of red, green & blue
+# Then set pixels to be black if less than half the found value, white if higher
+
+    @img3 =  img2.quantize(2, Magick::GRAYColorspace)
+    @img = @img3.despeckle
+
+    maxR=0
+    maxG=0
+    maxB=0
     @iPixels = @img.get_pixels(0,0,@img.columns,@img.rows)
+
+    @iPixels.each do |pixie|
+      if pixie.red > maxR
+        maxR=pixie.red
+      end
+      if pixie.green > maxG
+        maxG=pixie.green
+      end
+      if pixie.blue > maxB
+        maxB=pixie.blue
+      end
+    end
+
+    midRGB=maxR
+    if maxG<midRGB
+      midRGB=maxG
+    end
+    if maxB<midRGB
+      midRGB=maxB
+    end
+    midRGB=midRGB/2
+
+    @iPixels.each do |pixie|
+      if (pixie.red < midRGB) and (pixie.green < midRGB) and (pixie.blue < midRGB)
+        pixie.red = 0
+        pixie.green = 0
+        pixie.blue = 0
+      else
+        pixie.red = 65535
+        pixie.green = 65535
+        pixie.blue = 65535
+      end
+    end
+
+    @iPixels.each_index do |pixie|
+      x,y = getXY(pixie)
+      if y>0 and y<@img.rows-1 and x>0 and x<@img.columns-1
+        if @iPixels[pixie].red != @iPixels[getIndexXY(x-1,y-1)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x,y-1)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x+1,y-1)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x-1,y)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x+1,y)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x-1,y+1)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x,y+1)].red and \
+           @iPixels[pixie].red != @iPixels[getIndexXY(x+1,y+1)].red
+          puts("REPLACED")
+          @iPixels[pixie].red = @iPixels[getIndexXY(x-1,y)].red
+          @iPixels[pixie].green = @iPixels[getIndexXY(x-1,y)].green
+          @iPixels[pixie].blue = @iPixels[getIndexXY(x-1,y)].blue
+        end
+      end
+    end
+
+    puts("maxR = #{maxR}")
+    puts("maxG = #{maxG}")
+    puts("maxB = #{maxB}")
+
     @objs = Array.new
   end
 
@@ -82,7 +155,49 @@ class InputSource
     puts("Number of objects: #{@objs.length}")
     @objs.each do |ob|
       ob.listob
+#      puts(ob.printAsStrings)
     end
+    printOut
+  end
+
+  def printOut
+    outChars=['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z','1','2','3','4','5','6','7','8','9','0','!','@','£','$','%','^','&','*','(',')','-','_','=','+','[','{',']','}',';',':','|',',','<','.','>','/','?','~','`','±','§','#','™','‹','›','ﬁ','ﬂ','‡','°','Æ','Ú','»','¿','˘','¯','Ÿ']
+    outCount=0
+
+    theOut=@objs[0].printAsStrings     
+    theOut.each do |tO|
+      tO.tr!('X',outChars[outCount])
+    end
+    outCount += 1
+
+    @objs.each_index do |blobs|
+      if blobs > 0
+        tmpOut=@objs[blobs].printAsStrings     
+        tmpOut.each_index do |tO|
+          t0=tmpOut[tO]
+          t9=theOut[tO]
+
+          t0.tr!('X',outChars[outCount])
+          tZ=''
+          cLen=0
+          begin
+            if t0[cLen] == ' '
+              tZ = tZ + t9[cLen]
+            else
+              tZ = tZ + t0[cLen]
+            end
+            cLen += 1
+          end while cLen < t0.length
+          theOut[tO]=tZ
+        end
+        outCount += 1
+        if outCount >= outChars.length
+          outCount=0
+        end
+      end
+    end
+
+    puts(theOut)
   end
 
   def printsize()
@@ -157,7 +272,7 @@ class InputSource
           plist = nplist
         end while plist.length > 0
         @objs.push(@currentObj)
-        puts(@currentObj.printAsStrings)
+#        puts(@currentObj.printAsStrings)
       end
     end
   end
